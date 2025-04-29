bae73ef60f810d81434587bc6566e715c8e594b8
ArisSBG <125900474+ArisSBG@users.noreply.github.com>
2025-04-29 10:41:19 +0000

    premier test de solution

--- index.js
+++ index.js
@@ -5,6 +5,36 @@
 var express = require('express');
 var app = express();
 
+
+app.get("/api/:date?", function (req, res)  {
+  let date = req.params.date;
+
+  if(!date){
+     date = new Date();
+  }else {
+    // Si la date est numérique, c'est un timestamp UNIX
+    if (!isNaN(date)) {
+      date = new Date(parseInt(date));
+    } else {
+      // Sinon on tente de parser la date au format ISO
+      date = new Date(date);
+    }
+  }
+
+  // Si la date n'est pas valide
+  if (dateInput.toString() === 'Invalid Date') {
+    return res.json({ error: 'Invalid Date' });
+  }
+
+  // Retourne le format UNIX et UTC
+  const unix = dateInput.getTime();
+  const utc = dateInput.toUTCString();
+
+  res.json({ unix, utc });
+  
+  
+})
+
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
